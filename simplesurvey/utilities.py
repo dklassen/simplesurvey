d2a616b47f3cd5413f6056b921a408400cbde8b7
dklassen <dana.klassen@shopify.com>
2017-01-19 20:08:49 -0500

    Update ordinal format

--- simplesurvey/utilities.py
+++ simplesurvey/utilities.py
@@ -2,7 +2,8 @@ import math
 
 
 def to_ordinal(n):
-    return "%d%s" % (n, "tsnrhtdd"[(math.floor(n / 10) % 10 != 1) * (n % 10 < 4) * n % 10::4])
+    return "%d%s" % (
+        n, "tsnrhtdd"[(math.floor(n / 10) % 10 != 1) * (n % 10 < 4) * n % 10::4])
 
 
 def percent_of(data, column):
