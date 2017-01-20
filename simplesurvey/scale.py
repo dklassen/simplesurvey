2d518a3c876a47a8461dc05acd6182897758b7d3
dklassen <dana.klassen@shopify.com>
2017-01-19 20:34:28 -0500

    Add ordinal scale

--- simplesurvey/scale.py
+++ simplesurvey/scale.py
@@ -1,20 +1,20 @@
+import yaml
 
 
 class OrdinalScale:
-    """ Represents a ordinal scale with a mapping to a numerical value"""
 
     def __init__(self, labels=None, ratings=None, default_value=0):
+        assert len(labels) == len(ratings), "All labels need an associated rating"
+
         if labels is None:
             self._labels = []
+        self._labels = labels
 
         if ratings is None:
             self._ratings = []
-
-        assert len(labels) == len(ratings), "All labels need an associated rating"
-
-        self._labels = labels
         self._ratings = ratings
-        self._default_value = 0
+
+        self._default_value = default_value
 
     @property
     def ratings(self):
@@ -26,3 +26,11 @@ class OrdinalScale:
 
     def scoring(self):
         return {k: v for k, v in zip(self.labels, self.ratings)}
+
+
+def ordinal_scale_constructor(loader, node):
+    values = loader.construct_mapping(node)
+    return OrdinalScale(labels=values.get("labels"),
+                        ratings=values.get("ratings"))
+
+yaml.add_constructor("!OrdinalScale", ordinal_scale_constructor)
